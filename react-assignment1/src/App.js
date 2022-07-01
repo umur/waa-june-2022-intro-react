a5e13d2459aa109115e96e84de2b7338750104f6
ashish-pokhrel <ashishpokhrel45@gmail.com>
2022-07-01 17:42:44 -0500

    Sign In Page

--- react-assignment1/src/App.js
+++ react-assignment1/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import LoginPage from './component/login';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import './style/main.css'
 
 function App() {
   return (
-    <div className="App">
-     <LoginPage></LoginPage>
-    </div>
+
+    <div className="body text-center App">
+      <main className="form-signin">
+       <LoginPage></LoginPage>
+      </main>
+      </div> 
   );
 }
 
--- react-assignment1/src/component/login.js
+++ react-assignment1/src/component/login.js
@@ -1,31 +1,42 @@
 import React, { useState } from "react";
 
 
-
-const initialState = {
-    'username': null,
-    'password': null
-}
-
-
-function setValues(e) {
-    debugger
-    // setLoginState([e.target.name] = e.target.value )
-}
-
 function LoginPage() {
-    let [getLoginState, setLoginState] = useState(initialState);
+    let [getusername, setusername] = useState('');
+    let [getpassword, setpassword] = useState('');
+
+    const setValues = (e) => {
+        const targetName =e.target.name;
+        switch (targetName) {
+            case 'username':
+                setusername(e.target.value);
+                break;
+            case 'password':
+                setpassword(e.target.value);
+                break;
+        }
+    }
+
+
     return (
         <div>
-            <h1>SIGN IN</h1>
             <form>
-                <label>
-                    User Name:
-                    <input type="text" value={getLoginState.username} name="username" onChange={(setValues)} />
-                </label>
-                <p> This is the paragraph : {getLoginState.username}</p>
-                <input type="submit" value="Submit" />
+                <img className="mb-4" src={require('../resources/img/smile.jpg')} alt="" width="98" height="75" />
+                <h1 className="h3 mb-3 fw-normal">PLEASE SIGN IN</h1>
+                <div className="form-floating">
+                    <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" value={getusername} name="username" onChange={setValues} />
+                    <label htmlFor="floatingInput">Email address</label>
+                </div>
+                <div className="form-floating">
+                    <input type="password" className="form-control" id="floatingPassword" placeholder="Password" value={getpassword} name="password" onChange={setValues} />
+                    <label htmlFor="floatingPassword">Password</label>
+                </div>
+                <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+                <p className="mt-5 mb-3 text-muted">&copy; Ashish Pokhrel</p>
             </form>
+            <p>
+                {getpassword} hi {getusername}
+            </p>
         </div>
 
     )
