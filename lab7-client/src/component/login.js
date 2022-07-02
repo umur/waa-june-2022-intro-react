b77cee669a7f791a38da75b18e37a52c49410e9d
AKay-Cee <95042223+AKay-Cee@users.noreply.github.com>
2022-07-01 21:52:37 -0500

    login page added

---
+++ lab7-client/src/component/login.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function login() {
+    return (
+        <div>
+            <h1>Login</h1>
+            <form>
+                <label>Username:</label>
+                <input type="text"></input>
+                <br></br>
+                <label>Password:</label>
+                <input type="password"></input>
+            </form>
+        </div>
+    )
+}
