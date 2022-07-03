1ec69a75860329bff970b3cc2a2af8ce76e65258
Robeil Aregawi <silukeen1@gmail.com>
2022-07-02 22:36:03 -0500

    added

--- lab_7/src/App.js
+++ lab_7/src/App.js
@@ -1,13 +1,10 @@
 
 import './App.css';
 import React, { useEffect, useState } from 'react';
-import LoginForm from './components/login'
 import Signup from './components/signup';
-import HomePage from './components/homePage'
 import Product from './components/product';
 import Login from './components/login';
 import axios from 'axios';
-import Users from './components/Users'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 
@@ -29,16 +26,16 @@ function App() {
     fetchUser();
   },[])
 
-  let [userState, setUserState] = useState({ initialState });
+   let [userState, setUserState] = useState({ initialState });
 
 
   return (
     <div className="container-fluid">
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login/>}></Route>
-        <Route path="/signup" element={<Signup/>}></Route>
-        <Route path="/product" element={<Product/>}></Route>
+        <Route path="/"element={<Login/>}></Route>
+        <Route path="/Signup" element={<Signup/>}></Route>
+        <Route path="/Product" element={<Product/>}></Route>
       </Routes>
     </BrowserRouter>
      </div>
