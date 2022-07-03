6a20675373ea702f66f588bab36857f0df242120
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2022-07-02 21:24:23 -0500

    Added ListProduct and updated App.js

--- lab7-react-intro/src/App.js
+++ lab7-react-intro/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import SignIn from './component/SignIn';
 import SignUp from './component/SignUp';
 import AddProduct from './component/AddProduct';
+import ListProduct from './component/ListProduct';
+import { Container } from '@mui/system';
 
 function App() {
   return (
@@ -10,6 +12,10 @@ function App() {
       <SignIn />
       <SignUp />
       <AddProduct />
+      <Container>
+      <ListProduct />
+      </Container>
+      
     </div>
   );
 }
